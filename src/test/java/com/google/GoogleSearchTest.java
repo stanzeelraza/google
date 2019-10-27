b04a4e3b0342b8ddd78b75e67f5ecd2fa82d7a92
TanzeelRaza <tanzeel.raza@googlemail.com>
2019-10-27 16:17:45 -0400

    Different browser- cross browser

--- src/test/java/com/google/GoogleSearchTest.java
+++ src/test/java/com/google/GoogleSearchTest.java
@@ -10,11 +10,16 @@ import org.testng.annotations.Test;
 
 import org.testng.annotations.BeforeTest;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
 
@@ -23,14 +28,38 @@ public class GoogleSearchTest {
   SearchPage searchPage;
   
   @BeforeTest
-  public void beforeTest() {
-	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\raza_\\Downloads\\Jar files\\chromedriver.exe");
+  public void beforeTest() throws IOException {
 	  
-	  ChromeOptions options = new ChromeOptions();
-	  options.addArguments("start-maximized");
+	  Properties browserConfigProps = new Properties();
+  	FileInputStream fis = new FileInputStream("BrowserConfig.Txt");
+  	
+  	browserConfigProps.load(fis);
+  
+  	String browserType = browserConfigProps.getProperty("BROWSER");
+  	
+  	if (browserType.equalsIgnoreCase("CHROME")) {
+  		System.setProperty("webdriver.chrome.driver", "C:\\Users\\raza_\\Downloads\\Jar files\\chromedriver.exe ");
+  		driver = new ChromeDriver();
+  	}else if(browserType.equalsIgnoreCase("FIREFOX")) {
+  		System.setProperty("webdriver.chrome.driver", "C:\\Users\\raza_\\Downloads\\Jar files\\geckodriver.exe ");
+  		driver = new FirefoxDriver();
+  	}else if(browserType.equalsIgnoreCase("IE")) {
+  		System.setProperty("webdriver.chrome.driver", "C:\\Users\\raza_\\Downloads\\Jar files\\IEDriverServer.exe ");
+  		driver = new InternetExplorerDriver();
+  	}
 	  
-	  driver = new ChromeDriver(options);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  
+	  
+	  
+	  
+	  
+	  //System.setProperty("webdriver.chrome.driver", "C:\\Users\\raza_\\Downloads\\Jar files\\chromedriver.exe");
+	  
+	  //ChromeOptions options = new ChromeOptions();
+	  //options.addArguments("start-maximized");
+	  
+	  //driver = new ChromeDriver(options);
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		driver.get("https://www.google.com");
 		searchPage = PageFactory.initElements(driver, SearchPage.class);
